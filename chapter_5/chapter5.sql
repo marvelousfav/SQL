6af7b880dcfbbcb73d1cba0f1f60ea1cea962326
marvek16 <marvelousfav1603@gmail.com>
2024-07-31 11:06:40 +0200

    .

---
+++ chapter_5/chapter5.sql
@@ -0,0 +1,8 @@
+-- chapter5 
+
+-- Listing 5-1: Basic addition, subtraction and multiplication with SQL
+
+SELECT 2 + 2;    -- addition
+SELECT 9 - 1;    -- subtraction
+SELECT 3 * 4;    -- multiplication
+
