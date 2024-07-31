c84277d23b0c54460c22537c76057f84f4293cb4
marvek16 <marvelousfav1603@gmail.com>
2024-07-31 11:16:50 +0200

    .

--- chapter_5/chapter5.sql
+++ chapter_5/chapter5.sql
@@ -13,3 +13,18 @@ SELECT 11 % 6;   -- modulo division
 SELECT 11.0 / 6; -- decimal division
 SELECT CAST(11 AS numeric(3,1)) / 6;
 
+-- Listing 5-3: Exponents, roots and factorials with SQL
+
+SELECT 3 ^ 4;    -- exponentiation
+SELECT |/ 10;    -- square root (operator)
+SELECT sqrt(10); -- square root (function)
+SELECT ||/ 10;   -- cube root
+SELECT factorial (4);      -- factorial
+
+-- Order of operations
+
+SELECT 7 + 8 * 9; 	-- answer: 79
+SELECT (7 + 8) * 9;	-- answer: 135
+
+SELECT 3 ^ 3 - 1;   -- answer: 26
+SELECT 3 ^ (3 - 1); -- answer: 9
