9a3d88b1347c7518c2092226eaac4fbfe90f7b14
marvek16 <marvelousfav1603@gmail.com>
2024-07-31 11:14:56 +0200

    .

--- chapter_5/chapter5.sql
+++ chapter_5/chapter5.sql
@@ -6,3 +6,10 @@ SELECT 2 + 2;    -- addition
 SELECT 9 - 1;    -- subtraction
 SELECT 3 * 4;    -- multiplication
 
+-- Listing 5-2: Integer and decimal division with SQL
+
+SELECT 11 / 6;   -- integer division
+SELECT 11 % 6;   -- modulo division
+SELECT 11.0 / 6; -- decimal division
+SELECT CAST(11 AS numeric(3,1)) / 6;
+
