f047614e6771913f83ec7c4ebf9352e565c4877a
marvek16 <marvelousfav1603@gmail.com>
2024-07-31 11:20:28 +0200

    .

--- chapter_5/chapter5.sql
+++ chapter_5/chapter5.sql
@@ -28,3 +28,26 @@ SELECT (7 + 8) * 9;	-- answer: 135
 
 SELECT 3 ^ 3 - 1;   -- answer: 26
 SELECT 3 ^ (3 - 1); -- answer: 9
+
+-- Listing 5-4: Selecting Census population columns by race with aliases
+
+SELECT geo_name,
+       state_us_abbreviation AS "st",
+       p0010001 AS "Total Population",
+       p0010003 AS "White Alone",
+       p0010004 AS "Black or African American Alone",
+       p0010005 AS "Am Indian/Alaska Native Alone",
+       p0010006 AS "Asian Alone",
+       p0010007 AS "Native Hawaiian and Other Pacific Islander Alone",
+       p0010008 AS "Some Other Race Alone",
+       p0010009 AS "Two or More Races"
+FROM us_counties_2010;
+
+-- Listing 5-5: Adding two columns in us_counties_2010
+
+SELECT geo_name,
+       state_us_abbreviation AS "st",
+       p0010003 AS "White Alone",
+       p0010004 AS "Black Alone",
+       p0010003 + p0010004 AS "Total White and Black"
+FROM us_counties_2010;
