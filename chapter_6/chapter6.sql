5b4cbd8d84581ef9f2f15765e43dede3fa8693de
marvek16 <marvelousfav1603@gmail.com>
2024-08-01 09:51:54 +0200

    .

--- chapter_6/chapter6.sql
+++ chapter_6/chapter6.sql
@@ -74,3 +74,9 @@ ON schools_left.id = schools_right.id;
 SELECT *
 FROM schools_left INNER JOIN schools_right
 ON schools_left.id = schools_right.id;
+
+-- Listing 6-5: Using LEFT JOIN
+
+SELECT *
+FROM schools_left LEFT JOIN schools_right
+ON schools_left.id = schools_right.id;
