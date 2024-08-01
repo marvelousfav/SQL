5501705408c9dbb079e494cde512acf1a500d8f6
marvek16 <marvelousfav1603@gmail.com>
2024-08-01 12:44:53 +0200

    .

--- chapter_7/chapter7.sql
+++ chapter_7/chapter7.sql
@@ -45,3 +45,44 @@ VALUES(775, '2017/01/23', 'Y');
 
 INSERT INTO natural_key_composite_example (student_id, school_day, present)
 VALUES(775, '2017/01/23', 'N');     -- when the date is the same you get an error
+
+-- Listing 7-5: Declaring a bigserial column as a surrogate key
+
+CREATE TABLE surrogate_key_example (
+    order_number bigserial,
+    product_name varchar(50),
+    order_date date,
+    CONSTRAINT order_key PRIMARY KEY (order_number)
+);
+
+INSERT INTO surrogate_key_example (product_name, order_date)
+VALUES ('Beachball Polish', '2015-03-17'),
+       ('Wrinkle De-Atomizer', '2017-05-22'),
+       ('Flux Capacitor', '1985-10-26');
+
+SELECT * FROM surrogate_key_example;
+
+-- Listing 7-6: A foreign key example
+
+CREATE TABLE licenses (
+    license_id varchar(10),
+    first_name varchar(50),
+    last_name varchar(50),
+    CONSTRAINT licenses_key PRIMARY KEY (license_id)
+);
+
+CREATE TABLE registrations (
+    registration_id varchar(10),
+    registration_date date,
+    license_id varchar(10) REFERENCES licenses (license_id),
+    CONSTRAINT registration_key PRIMARY KEY (registration_id, license_id)
+);
+
+INSERT INTO licenses (license_id, first_name, last_name)
+VALUES ('T229901', 'Lynn', 'Malero');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A203391', '3/17/2017', 'T229901');
+
+INSERT INTO registrations (registration_id, registration_date, license_id)
+VALUES ('A75772', '3/17/2017', 'T000001');
