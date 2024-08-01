659b7eee45d870fd944a92667414a8b8eca3524c
marvek16 <marvelousfav1603@gmail.com>
2024-08-01 09:09:14 +0200

    .

--- chapter_6/chapter6.sql
+++ chapter_6/chapter6.sql
@@ -31,3 +31,34 @@ VALUES
     ('Lee', 'Smith', 59300, 1),
     ('Soo', 'Nguyen', 83000, 2),
     ('Janet', 'King', 95000, 2);
+
+-- Listing 6-2: Joining the employees and departments tables
+
+SELECT *
+FROM employees JOIN departments
+ON employees.dept_id = departments.dept_id;
+
+-- Listing 6-3: Creating two tables to explore JOIN types
+
+CREATE TABLE schools_left (
+    id integer CONSTRAINT left_id_key PRIMARY KEY,
+    left_school varchar(30)
+);
+
+CREATE TABLE schools_right (
+    id integer CONSTRAINT right_id_key PRIMARY KEY,
+    right_school varchar(30)
+);
+
+INSERT INTO schools_left (id, left_school) VALUES
+    (1, 'Oak Street School'),
+    (2, 'Roosevelt High School'),
+    (5, 'Washington Middle School'),
+    (6, 'Jefferson High School');
+
+INSERT INTO schools_right (id, right_school) VALUES
+    (1, 'Oak Street School'),
+    (2, 'Roosevelt High School'),
+    (3, 'Morrison Elementary'),
+    (4, 'Chase Magnet Academy'),
+    (6, 'Jefferson High School');
