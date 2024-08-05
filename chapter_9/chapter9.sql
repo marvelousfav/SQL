07d924386dd6adce80674264ca9fa538995f88aa
marvek16 <marvelousfav1603@gmail.com>
2024-08-05 10:19:23 +0200

    .

--- chapter_9/chapter9.sql
+++ chapter_9/chapter9.sql
@@ -94,3 +94,23 @@ ALTER TABLE meat_poultry_egg_inspect ADD COLUMN st_copy varchar(2);
 UPDATE meat_poultry_egg_inspect
 SET st_copy = st;
 
+-- Listing 9-10: Checking values in the st and st_copy columns
+
+SELECT st,
+       st_copy
+FROM meat_poultry_egg_inspect
+ORDER BY st;
+
+-- Listing 9-11: Updating the st column for three establishments
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'MN'
+WHERE est_number = 'V18677A';
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'AL'
+WHERE est_number = 'M45319+P45319';
+
+UPDATE meat_poultry_egg_inspect
+SET st = 'WI'
+WHERE est_number = 'M263A+P263A+V263A';
