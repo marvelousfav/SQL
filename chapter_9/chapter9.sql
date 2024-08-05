0b99cc8169761ab1e2a4c8fa119417cc96f8efac
marvek16 <marvelousfav1603@gmail.com>
2024-08-05 10:13:45 +0200

    .

---
+++ chapter_9/chapter9.sql
@@ -0,0 +1,26 @@
+-- chapter 9
+
+-- Listing 9-1: Importing the FSIS Meat, Poultry, and Egg Inspection Directory
+-- https://catalog.data.gov/dataset/meat-poultry-and-egg-inspection-directory-by-establishment-name
+
+CREATE TABLE meat_poultry_egg_inspect (
+    est_number varchar(50) CONSTRAINT est_number_key PRIMARY KEY,
+    company varchar(100),
+    street varchar(100),
+    city varchar(30),
+    st varchar(2),
+    zip varchar(5),
+    phone varchar(14),
+    grant_date date,
+    activities text,
+    dbas text
+);
+
+COPY meat_poultry_egg_inspect
+FROM 'C:\bootcamp.docs\5_SQL\my_work\chapter_9\MPI_Directory_by_Establishment_Name.csv'
+WITH (FORMAT CSV, HEADER, DELIMITER ',');
+
+CREATE INDEX company_idx ON meat_poultry_egg_inspect (company);
+
+-- Count the rows imported:
+SELECT count(*) FROM meat_poultry_egg_inspect;
